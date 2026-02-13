3b41cbe4ce0cf6d69d348a5950ca6183d16076a1
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-13 18:47:15 +0000

    esc drcfgv

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+let school = {
+    'name',
+    'average class size', 
+    'mascot',
+};
+let Yada = "${name} has an average class size of ${average class size} and their mascot is a ${mascot}";
 
+document.getElementById('result').innerHTML = Yada;
+
+//should create an object named school that has three properties: 
+// name, average class size, and mascot
+
+//should assign the innerHTML of 
+// the HTML element with the 
+// id result to the  string 
+// "${name} has an average class size 
+// of ${average class size} and 
+// their mascot is a ${mascot}"
